c3228e04640089ddb18ee33d7dea0fda9b5f5605
Oliver Mentel <oliver.mentel@swisscom.com>
2020-05-04 15:24:59 +0200

    added functions

---
+++ GameOfLife/src/Game/Main.java
@@ -0,0 +1,48 @@
+public class Game {
+
+    // global definierte Konstanten für die beiden Dimensionen
+    final static int DIM1 = 12;
+    final static int DIM2 = 12;
+
+    // liefert eine zufällig initialisierte Welt
+    public static boolean[][] initWelt() {
+        return initWelt();
+    }
+
+
+    // gibt die aktuelle Welt aus
+    public static void zeigeWelt(boolean[][] welt) {
+
+    }
+
+    // wendet die 4 Regeln an und gibt die
+    // Folgegeneration wieder zurück
+    public static boolean[][] wendeRegelnAn(boolean[][] welt) {
+        return wendeRegelnAn(welt);
+    }
+
+    public static int anzNachbarn(boolean[][] welt, int x, int y) {
+        int ret = 0;
+        for (int i = x - 1; i <= x + 1; ++i)
+            for (int j = y - 1; j <= y + 1; ++j)
+                if (welt[i][j])
+                    ret += 1;
+        // einen Nachbarn zuviel mitgezählt?
+        if (welt[x][y])
+            ret -= 1;
+        return ret;
+    }
+
+    public static void main(String[] args) {
+        boolean[][] welt = initWelt();
+        System.out.println("Startkonstellation");
+        zeigeWelt(welt);
+        for (int i = 1; i <= 100; i++) {
+            welt = wendeRegelnAn(welt);
+            System.out.println("Generation " + i);
+            zeigeWelt(welt);
+
+
+        }
+    }
+}
